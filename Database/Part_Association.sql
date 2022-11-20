1285a18edba2385c7ebdf177458cd4e0f1435cbd
triv117 <tomriv117@outlook.com>
2022-11-20 15:56:33 -0500

    Minor DB Fix

--- Database/Part_Association.sql
+++ Database/Part_Association.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Part_Association]
 (
-	[machine_id] INT NOT NULL PRIMARY KEY, 
-    [part_id] INT NOT NULL PRIMARY KEY, 
+	[machine_id] INT NOT NULL, 
+    [part_id] INT NOT NULL, 
     CONSTRAINT [FK_Parts_Machine] FOREIGN KEY ([machine_id]) REFERENCES [Item]([item_id]),
-    CONSTRAINT [FK_Parts_Part] FOREIGN KEY ([part_id]) REFERENCES [Item]([item_id])
+    CONSTRAINT [FK_Parts_Part] FOREIGN KEY ([part_id]) REFERENCES [Item]([item_id]),
+    CONSTRAINT [PK_Parts] PRIMARY KEY ([machine_id],[part_id])
 )
