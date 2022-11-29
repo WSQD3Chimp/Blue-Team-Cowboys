17515b22f4c459668b1383e8543859dd9e9cc10b
triv117 <tomriv117@outlook.com>
2022-11-29 10:52:57 -0500

    Added Connection Class

---
+++ DBConnect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace BlueTeamProject
+{
+    internal class DBConnect
+        {
+        static string constr;
+        static SqlConnection conn;
+
+        public static void Connect()
+        {
+            constr = @"Data Source=(localdb)\ProjectModels;Initial Catalog=Database;Integrated Security=True;Pooling=False;Connect Timeout=30";
+            conn = new SqlConnection(constr);
+            conn.Open();
+        }
+
+        public static void Close()
+        {
+            conn.Close();
+        }
+    }
+}
