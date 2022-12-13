eee572d4f73fbe226a33da1833bca1d260f01ba2
WSQD3Chimp <70527945+WSQD3Chimp@users.noreply.github.com>
2022-12-12 19:59:40 -0500

    Revert "Revert "Transactions""
    This reverts commit ccba920987f5fa67e8ca2f9d2c2baa2ce2bb254c.

--- ViewTransactionForm.cs
+++ ViewTransactionForm.cs
@@ -11,10 +11,30 @@ using System.Windows.Forms;
 namespace BlueTeamProject
 {
     public partial class ViewTransactionForm : Form
-    {
-        public ViewTransactionForm()
+    {   
+        Transaction transaction;
+        public ViewTransactionForm(int id)
         {
             InitializeComponent();
+            this.transaction = DBController.viewTransaction(id);
+
+            TransactionIDText.Text = transaction.transaction_id.ToString();
+            TransactionDate.Text = transaction.date.ToString();
+            TransactionChangesBy.Text = transaction.account_id.ToString();
+            TransactionOperation.Text = transaction.operation_type;
+            TransactionAffected.Text = transaction.item_id.ToString();
+            if (transaction.unit_change!=0)
+            {
+                TransactionChange.Text = transaction.unit_change.ToString();  
+            }
+        }
+
+        private void GoBackViewTransaction_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var transactions = new TransactionListForm(DBController.viewTransactions());
+            transactions.FormClosed += (s, args) => this.Close();
+            transactions.Show();
         }
     }
 }
